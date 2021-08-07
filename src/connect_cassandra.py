6061ada42f5398fd1fe1cd890cddd0ca25c938d5
Daryl <daryl.psh@gmail.com>
2021-08-07 23:51:58 +0800

    Helper function to connect to cassandra

--- src/connect_cassandra.py
+++ src/connect_cassandra.py
@@ -1,21 +1,49 @@
-from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, Session
 import pandas as pd
 
 
-def pandas_factory(colnames, rows):
+def pandas_factory(colnames, rows) -> pd.DataFrame:
+    """
+    Helper function to extract data from Cassandra in a pandas DataFrame format
+    Source: https://stackoverflow.com/questions/41247345/python-read-cassandra-data-into-pandas
+
+    Args:
+        colnames ([type]): [description]
+        rows ([type]): [description]
+
+    Returns:
+        DataFrame
+    """
     return pd.DataFrame(rows, columns=colnames)
 
 
-keyspace = "social_media"
+def connect_to_database(keyspace: str) -> Session:
+    """
+    Connect to a Cassandra keyspace
 
-cluster = Cluster()
-session = cluster.connect(keyspace)
-session.row_factory = pandas_factory
-session.default_fetch_size = None
+    Args:
+        keyspace (str): keyspace to connect to
+    """
+    cluster = Cluster()
+    session = cluster.connect(keyspace)
+    session.row_factory = pandas_factory
+    session.default_fetch_size = None
 
-query = "SELECT * FROM tweets LIMIT 5"
+    return session
 
-rows = session.execute(query, timeout=None)
-df = rows._current_rows
 
-print(df)
+def extract_data(session: Session, query: str) -> pd.DataFrame:
+    """
+    Extract all the data from a particular table in Cassandra
+
+    Args:
+        session (Session): Database session
+        query (str): Query to be executed
+
+    Returns:
+        DataFrame: Data from Cassandra in Pandas DataFrame format
+    """
+    rows = session.execute(query, timeout=None)
+    df = rows._current_rows
+
+    return df
