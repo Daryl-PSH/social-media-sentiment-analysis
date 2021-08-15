aaa5f10504d15073e0c34fa04715b66a84df2f55
Daryl <daryl.psh@gmail.com>
2021-08-15 21:13:12 +0800

    Removed forms.py

--- src/app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField
-
-
-class TickerForm(FlaskForm):
-    ticker = StringField("Ticker")
-    start_date = DateField("StartDate")
-    end_date = DateField("EndDate")
-    submit = SubmitField("Generate")
